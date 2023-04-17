22be05c8ae34b4ad0045ded6fce10a222ba78d54
Julia <matterfixed@gmail.com>
2023-04-17 10:14:39 +0300

    done

---
+++ hw_seminar1_2.java
@@ -0,0 +1,15 @@
+// Вывести все простые числа от 1 до 1000
+
+import java.util.Scanner;
+
+public class hw_seminar1_2 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a = 1;
+        int num = 0;
+        while (num < 1000) {
+            num = num +1;
+            System.out.println(num);
+        }
+    }
+}
