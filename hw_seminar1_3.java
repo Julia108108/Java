5ec854826d3181a221467279413a85fb2066e718
Julia <matterfixed@gmail.com>
2023-04-19 11:56:25 +0300

    done

--- hw_seminar1_3.java
+++ hw_seminar1_3.java
@@ -1,14 +1,15 @@
 //  Вычислить n!(произведение чисел от 1 до n)
 
 import java.util.Scanner;
-public class hw_seminar1_3 {
-    public long factorialUsingForLoop(int n) {
-        long fact = 1;
-        for (int i = 2; i <= n; i++) {
-            fact = fact * 1;
-        }
-        return fact;
-        //System.out.println("Произведение чисел от 1 до n: " + fact);
-    }
 
+public class hw_seminar1_3 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Введите число: ");
+        int num = sc.nextInt();
+        int result = 1;
+        for (int i = 1; i <= num; i++) result *= i;
+        System.out.println("n! равен " + result);
+    }
 }
+       
