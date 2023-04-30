2f3fbe512f6311dd01d93cb62fa4b6d99d6c0bcd
Julia <matterfixed@gmail.com>
2023-04-30 19:04:19 +0300

    done

--- hw_seminar3_task1.java
+++ hw_seminar3_task1.java
@@ -1,6 +1,30 @@
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Iterator;
+import java.util.Random;
+
 public class hw_seminar3_task1 {
     /*
     Пусть дан произвольный список целых чисел, удалить из него
     четные числа
      */
+    public static void main(String[] args) {
+        List numbers = new ArrayList();
+        Random random = new Random();
+
+        for (int i = 0; i < 10; i++) {
+            numbers.add(random.nextInt(100));
+        }
+
+        Iterator iterator = numbers.iterator();
+
+        while (iterator.hasNext()) {
+            int number = (int) iterator.next();
+                if (number % 2 == 0) {
+                    iterator.remove();
+                }
+        }
+
+        System.out.println(numbers);
+    }
 }
