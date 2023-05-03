436eda4bfdb78bb5e791a19c88e6e2abb34ad1aa
Julia <matterfixed@gmail.com>
2023-05-03 13:11:43 +0300

    done

--- seminar4_task1.java
+++ seminar4_task1.java
@@ -1,5 +1,5 @@
-import.java.util.ArrayList;
-import java.util.Collection;
+import java.util.ArrayList;
+import java.util.LinkedList;
 import java.util.List;
 
 public class seminar4_task1 {
@@ -7,9 +7,6 @@ public class seminar4_task1 {
 
     public static void main(String[] args) {
         
-    }
-
-    static void ex0(){
         /*
         1) Замерьте время, за которое в ArrayList добавятся 10000 элементов.
         2) Замерьте время, за которое в LinkedList добавятся 10000 элементов. Сравните с предыдущим.
@@ -21,7 +18,8 @@ public class seminar4_task1 {
         for (int i = 0; i < 100000; i++) {
             list2.add(i);
         }
-        long timeFinish = System.curentTimeMillis();
+        long timeFinish = System.currentTimeMillis();
+        long timeStart = System.currentTimeMillis();
         System.out.println(timeFinish - timeStart);
     }
 }
